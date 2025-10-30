f7295683c1ab720ad36c9893c096bd02425e7508
王筱冉 <ran920_2023@qq.com>
2025-10-30 16:15:35 +0800

    six

--- frontend/admin/src/App.vue
+++ frontend/admin/src/App.vue
@@ -4,6 +4,7 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
       <router-link to="/manager">图书管理员</router-link>
+      <router-link to="/test">读者</router-link>
     </nav>
     <router-view/>
   </div>
